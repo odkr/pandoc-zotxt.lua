3471f7efe0ae3c91ec80bcb47fad6a13f07e70e7
Odin Kroeger <odkr@users.noreply.github.com>
2021-05-28 19:30:11 +0200

    feat: No longer can be run as root.
    Also:
    
    * refact: Moved cleanup functions.
    * Cosmetic changes to `onexit` and `yesno`.

--- install.sh
+++ install.sh
@@ -119,7 +119,7 @@ onexit() {
 	trap '' EXIT ${TRAPS-INT TERM} || :
 	set +e
 	# shellcheck disable=2059
-	printf "${R-}\\033[K\\r"
+	printf "${R-}\\r\\033[K\\r"
 	if [ "${1-0}" -gt 0 ]; then
 		warn "${BL-}Caught ${B-}%s.${R-}\\033[K" "$(signame "$1")"
 	elif [ "$__ONEXIT_STATUS" -gt 0 ]; then
@@ -132,7 +132,7 @@ onexit() {
 		# shellcheck disable=2059
 		[ "${R-}" ] && printf "$R"
 		[ "${__ONEXIT_ERR-0}" -ne 0 ] && \
-			warn "${RD-}Clean-up failed.${R-}"
+			warn "${RD-}Clean-up failed.${R-}\\033[K"
 	fi
 	if [ "${1-0}" -gt 0 ]; then
 		__ONEXIT_STATUS=$((128+$1))
@@ -217,8 +217,8 @@ yesno() (
 
 	prompt="\"${B}yes$R\" or \"${B}no$R\""
 	help="(Press ${B}Enter$R to confirm or ${B}Ctrl$R-${B}c$R to abort.)"
-	cs=39
-	[ "${2-}" ] && cs=$((26 - ${#2}))
+	cs=51
+	[ "${2-}" ] && cs=$((38 - ${#2}))
 
 	exec 1>&2
 	printf -- "$NAME: $1\n"
@@ -230,8 +230,12 @@ yesno() (
 		[ "${2-}" ] && printf " [$B$2$R]"
 		printf -- ": \\n$NAME: $help\\033[1A\\033[${cs}D"
 		read -r rep || return 69
-		printf '\033[K\r'
-		: "${rep:="${2-}"}"
+
+		if ! [ "${rep-}" ] && [ "${2-}" ]; then
+			rep="$2"
+			printf "\\033[1A\\033[32C$BL%s$R\033[K\\n" "$rep"
+		fi
+		printf "$R\033[K\r"
 		case $rep in
 			([Yy]|[Yy][Ee][Ss]) return 0 ;;
 			([Nn]|[Nn][Oo])     return 1 ;;
@@ -269,252 +273,6 @@ pphome() {
 }
 
 
-# CLEANUP FUNCTIONS
-# =================
-
-# issafe - Determine whether a file is safe for removal
-#
-# Synopsis:
-#	issafe FILE DIR [PATTERN [PATTERN [...]]]
-#
-# Descriptions:
-#	FILE counts as safe to remove if and only if:
-#
-#	(1) FILE is an absolute path
-#	(2) FILE is a sub-directory of DIR.
-#	(3) FILE matches every given PATTERN. 
-#
-# Arguments:
-#	FILE	A file.
-#	DIR	A directory.
-#	PATTERN	A string.
-#
-# Returns:
-#	0	FILE is safe to be removed.
-#	1	FILE is *not* safe to be removed.
-#
-# Caveates:
-#	This function must *not* define its own sub-environment.
-#	dash fails to update $? for sub-shells called from a trap action.
-issafe() {
-	: "${1:?}"
-	: "${2:?}"
-
-	[ -e "$1" ] || return 0
-	[ -d "$2" ] || return 1
-	[ "$1" = "$2" ] && return 1
-	case $1 in
-		(/*)   : ;;
-		(*)    return 1
-	esac
-	case $1 in
-		($2/*) : ;;
-		(*)    return 1
-	esac
-
-	__ISSAFE_FNAME="$(basename "$1")"
-	shift 2
-	for __ISSAFE_PATTERN; do
-		case $__ISSAFE_FNAME in
-			(*$__ISSAFE_PATTERN*) : ;;
-			(*)                   return 1
-		esac
-	done
-
-	return 0
-}
-
-
-# rrm - Recursively remove a file or directory.
-#
-# Synopsis:
-#	rrm FILE
-#
-# Description:
-#	Recursively removes FILE, but only if:
-#	(1) FILE resides in $PANDOC_FILTER_DIR or
-#	    in one of its sub-directories.
-#	(2) FILE matches the pattern $FILTER.
-#
-#	Prints a message to STDERR if the file removed.
-#
-# Arguments:
-#	FILE	The file to be removed.
-#
-# Globals:
-#	PANDOC_FILTER_DIR	The directory where filters are located.
-#	FILTER			The name of the filter that is installed.
-#
-# Returns:
-#	0	FILE was removed.
-#	1	An error occurred.
-rrm() {
-	: "${1:?}"
-	: "${PANDOC_FILTER_DIR:?}"
-	: "${FILTER:?}"
-	issafe "$1" "$PANDOC_FILTER_DIR" "$FILTER" || return
-	warn "Removing $B%s$R." "$(pphome "$1")"
-	rm -rf "$1"
-}
-
-
-# srmdir - Safely remove a directory.
-#
-# Synopsis:
-#	srmdir DIR [PARENT PATTERN]
-#
-# Description:
-#	Removes a directory, but only if:
-#	(1) DIR is a sub-directory of PARENT.
-#	(2) DIR matches the pattern PATTERN.
-#
-#	Prints a message to STDERR if the directory is removed.
-#
-# Arguments:
-#	DIR	The directory to be removed.
-#
-#	PARENT	The parrent directory. Optional.
-#		Defaults to $PANDOC_FILTER_DIR.
-#
-#	PATTERN	A pattern that DIR must match. Optional.
-#		Defaults to $FILTER.
-#
-# Globals:
-#	PANDOC_FILTER_DIR	The directory where filters are located.
-#	FILTER			The name of the filter that is installed.
-#
-# Returns:
-#	0	DIR was removed.
-#	1	An error occurred.
-srmdir() {
-	: "${PANDOC_FILTER_DIR:?}"
-	: "${FILTER:?}"
-	case $# in
-		(0) return 1 ;;
-		(1) issafe "$1" "$PANDOC_FILTER_DIR" "$FILTER" || return ;;
-		(*) issafe "$@" || return
-	esac
-	warn "Removing $B%s$R." "$(pphome "$1")"
-	rmdir "$1"
-}
-
-
-# srmlink - Safely remove a link.
-#
-# Synopsis:
-#	srmlink LINK [DIR PATTERN]
-#
-# Description:
-#	Removes a LINK, but only if:
-#	(0) It *is* a LINK.
-#	(1) LINK is in DIR or one of its sub-directories.
-#	(2) LINK matches the pattern PATTERN.
-#
-# Arguments:
-#	LINK	The link to be removed.
-#
-#	DIR	A directory. Optional.
-#		Defaults to $PANDOC_FILTER_DIR.
-#
-#	PATTERN	A pattern that LINK must match. Optional.
-#		Defaults to $FILTER.
-#
-# Globals:
-#	PANDOC_FILTER_DIR	The directory where filters are located.
-#	FILTER			The name of the filter that is installed.
-#
-# Returns:
-#	0	DIR was removed.
-#	1	An error occurred.
-srmlink() {
-	: "${PANDOC_FILTER_DIR:?}"
-	: "${FILTER:?}"
-	case $# in
-		(0) return 1 ;;
-		(1) issafe "$1" "$PANDOC_FILTER_DIR" "$FILTER" || return ;;
-		(*) issafe "$@" || return
-	esac
-	[ -L "$1" ] || return
-	rm "$1"
-}
-
-
-# srmfile - Safely remove a file.
-#
-# Synopsis:
-#	srmfile FILE [DIR PATTERN]
-#
-# Description:
-#	Removes a FILE, but only if:
-#	(1) FILE is in DIR or one of its sub-directories.
-#	(2) FILE matches the pattern PATTERN.
-#
-# Arguments:
-#	FILE	The file to be removed.
-#
-#	DIR	A directory. Optional.
-#		Defaults to $PANDOC_FILTER_DIR.
-#
-#	PATTERN	A pattern that LINK must match. Optional.
-#		Defaults to $FILTER.
-#
-# Globals:
-#	PANDOC_FILTER_DIR	The directory where filters are located.
-#	FILTER			The name of the filter that is installed.
-#
-# Returns:
-#	0	DIR was removed.
-#	1	An error occurred.
-srmfile() {
-	: "${PANDOC_FILTER_DIR:?}"
-	: "${FILTER:?}"
-	case $# in
-		(0) return 1 ;;
-		(1) issafe "$1" "$PANDOC_FILTER_DIR" "$FILTER" || return ;;
-		(*) issafe "$@" || return
-	esac
-	rm "$1"
-}
-
-
-# restore - Safely replace a file with another one.
-#
-# Synopsis:
-#	restore BACKUP ORIGINAL [PATTERN [PATTERN [...]]]
-#
-# Description:
-#	Replaces ORIGINAL with BACKUP, but only if:
-#	(1) ORIGINAL and BACKUP are in $HOME or one of its sub-directories.
-#	(2) The filename of BACKUP contains that of ORIGINAL.
-#	(3) The filename of BACKUP matches every given PATTERN.
-#
-# Arguments:
-#	ORIGINAL	A filename.
-#	BACKUP		A filename.
-#	PATTERN		A pattern. Optional.
-#			Defaults to "backup" *and* $$ (i.e., two patterns).
-#
-# Returns:
-#	0	ORIGINAL was replaced with BACKUP.
-#	1	An error occurred.
-restore() (
-	: "${HOME:?}"
-	FR="${1:?}" TO="${2:?}"	
-	shift 2
-
-	fname="$(basename "$TO")"
-	case $# in
-		(0) issafe "$FR" "$HOME" "$fname" backup "$$" || return ;;
-		(*) issafe "$FR" "$HOME" "$fname" "$@" || return
-	esac
-	issafe "$TO" "$HOME" || return
-
-	warn "Restoring $B%s$R." "$(pphome "$TO")"
-	[ -L "$TO" ] && rm "$TO"
-	mv "$FR" "$TO"
-)
-
-
 # PRELUDE
 # =======
 
@@ -537,9 +295,14 @@ if [ -t 1 ]; then
 	esac
 fi
 
+
+
 PATH=/bin:/usr/bin
 PATH="$(getconf PATH):$PATH"
 export PATH
+
+[ "$(id -u)" -eq 0 ] && \
+	panic 64 "${RD}Refusing to run with superuser privileges.$R"
 
 : "${HOME:?}"
 : "${XDG_DATA_HOME:="$HOME/.local/share"}"
@@ -592,26 +355,38 @@ trapsig onexit 0 1 2 3 15
 
 if [ "$INSTALL_DIR" != "$REPO_DIR" ]; then
 	[ -e "$PANDOC_FILTER_DIR/$REPO" ] && \
-		panic 0 "$B$REPO$R: Already installed."
+		panic 0 "$B$REPO$R: ${RD}Already installed.$R"
 	[ -d "$REPO" ] || panic 69 "$B$REPO$R: ${RD}No such directory.$R"
 
 	# Create Pandoc filter directory if it does not exist yet. 
 	if ! [ -e "$PANDOC_FILTER_DIR" ]; then
+		rmdir_wrapper() {
+			: "${1:?}"
+			warn "Removing $B%s$R." "$(pphome "$1")"
+			rmdir "$1"
+		}
+
 		IFS=/ N=0 DIR=
 		for SEG in $PANDOC_FILTER_DIR; do
 			DIR="${DIR%/}/$SEG"
 			[ -e "$DIR" ] && continue
 			N=$((N + 1))
 			eval "readonly DIR_$N=\"\$DIR\""
-			EX="srmdir \"\$DIR_$N\" \"\$HOME\"; ${EX-}"
+			EX="rmdir_wrapper \"\$DIR_$N\" \"\$HOME\"; ${EX-}"
 			warn "Making directory $B%s$R." "$(pphome "$DIR")"
 			mkdir "$DIR" || exit 69
 		done
 		unset IFS
 	fi
 	
+	rm_install_dir() {
+		: "${INSTALL_DIR:?}"
+		warn "Removing $B%s$R." "$(pphome "$INSTALL_DIR")"
+		rm -rf "$INSTALL_DIR"
+	}
+
 	# Copy the files.
-	EX="rrm \"\$INSTALL_DIR\"; ${EX-}"
+	EX="rm_install_dir; ${EX-}"
 	warn "Copying $B%s$R to $B%s$R." \
 	     "$(pphome "$REPO")" "$(pphome "$PANDOC_FILTER_DIR")"
 	cp -R "$REPO" "$PANDOC_FILTER_DIR"
@@ -625,6 +400,14 @@ warn "Symlinking $B%s$R into $B%s$R." \
 	"$(pphome "$FILTER")" "$(pphome "$PANDOC_FILTER_DIR")"
 readonly FILTER_BACKUP="$PANDOC_FILTER_DIR/$FILTER.orig"
 if [ -e "$FILTER" ] || [ -L "$FILTER" ]; then
+	restore_old_filter() (
+		: "${FILTER_BACKUP:?}"
+		: "${FILTER_ORIG:?}"
+		warn "Restoring $B%s$R." "$(pphome "$FILTER_ORIG")"
+		[ -L "$FILTER_ORIG" ] && rm "$FILTER_ORIG"
+		mv "$FILTER_BACKUP" "$FILTER_ORIG"
+	)
+
 	if [ -f "$FILTER_BACKUP" ] && ! [ -L "$FILTER_BACKUP" ]; then
 		panic 69 "$B%s$R: ${RD}Refusing to overwrite.${R}" \
 			"$(pphome "$FILTER_BACKUP")"
@@ -635,14 +418,14 @@ if [ -e "$FILTER" ] || [ -L "$FILTER" ]; then
 
 	warn "Making a backup of the current $B$FILTER$R."
 	cp -PR "$FILTER" "$FILTER_BACKUP"
+	readonly FILTER_ORIG="$PANDOC_FILTER_DIR/$FILTER"
+	EX="restore_old_filter; ${EX-}"
+	CLEANUP="[ -L \"\$FILTER_BACKUP\"] && rm -- \"\$FILTER_BACKUP\"; \
+	         ${CLEANUP-}"
+else
 	# shellcheck disable=2034
 	readonly FILTER_ORIG="$PANDOC_FILTER_DIR/$FILTER"
-	EX="restore \"\$FILTER_BACKUP\" \"\$FILTER_ORIG\" orig; ${EX-}"
-	# The use of srmlink is on purpose, so that the script does not
-	# accidentally delete an old filter of the same name.
-	CLEANUP="srmlink \"\$FILTER_BACKUP\"; ${CLEANUP-}"
-else
-	EX="srmlink \"\$PANDOC_FILTER_DIR/\$FILTER\"; ${EX-}"
+	EX="[ -L \"\${FILTER_ORIG:?}\" ] && rm -- \"\$FILTER_ORIG\"; ${EX-}"
 fi
 ln -fs "$REPO/$FILTER" .
 
@@ -677,6 +460,13 @@ then
 fi
 
 if [ "${MODIFY_MANPATH-}" ]; then
+	mv_wrapper() {
+		: "${1:?}"
+		: "${2:?}"
+		warn "Restoring old $B%s$R." "$(pphome "$2")"
+		mv "$1" "$2"
+	}
+
 	NOW="$(date +%Y-%d-%mT%H-%M-%S)"
 	# shellcheck disable=2016
 	REL_MAN_DIR="$(pphome "$MAN_DIR" '$HOME')"
@@ -694,8 +484,8 @@ if [ "${MODIFY_MANPATH-}" ]; then
 		eval "readonly RC_$N=\"\$RC\""
 		eval "readonly RC_BACKUP_$N=\"\$BACKUP\""
 		cp "$RC" "$BACKUP"
-		EX="restore \"\$RC_BACKUP_$N\" \"\$RC_$N\"; ${EX-}"
-		CLEANUP="srmfile \"\$RC_BACKUP_$N\" \
+		EX="mv_wrapper \"\$RC_BACKUP_$N\" \"\$RC_$N\"; ${EX-}"
+		CLEANUP="rm -- \"\${RC_BACKUP_$N}\" \
 			\"\$HOME\" \"\$(basename \"\$RC_$N\")\"; ${CLEANUP-}"
 		printf '\n\n# Added by %s installer on %s.\n%s\n\n' \
 			"$FILTER" "${NOW%T*}" "$CODE" >>"$RC"
@@ -712,19 +502,34 @@ fi
 if [ ${RC_FOUND-} ] || [ -e "$LATEST_DIR" ]; then
 	warn "Symlinking $B$REPO$R to $B$FILTER-latest$R."
 	if [ -e "$LATEST_DIR" ] || [ -L "$LATEST_DIR" ]; then
+		restore_prev_latest() (
+			: "${LATEST_DIR_BACKUP:?}"
+			: "${LATEST_DIR:?}"
+			warn "Restoring $B%s$R." "$(pphome "$LATEST_DIR")"
+			[ -L "$LATEST_DIR" ] && rm "$LATEST_DIR"
+			mv "$LATEST_DIR_BACKUP" "$LATEST_DIR"
+		)
 		readonly LATEST_DIR_BACKUP="$LATEST_DIR-backup-pid$$"
 		[ -e "$LATEST_DIR_BACKUP" ] && ! [ -L "$LATEST_DIR_BACKUP" ] \
 			&& panic 69 "$B%s$R: ${RD}Not a symlink.${R}" \
 			            "$(pphome "$LATEST_DIR_BACKUP")"
 		cp -PR "$LATEST_DIR" "$LATEST_DIR_BACKUP"
 		mv "$LATEST_DIR" "$LATEST_DIR_BACKUP"
-		EX="restore \"\$LATEST_DIR_BACKUP\" \"\$LATEST_DIR\"; ${EX-}"
-		CLEANUP="srmlink \"\$LATEST_DIR_BACKUP\"; ${CLEANUP-}"
+		EX="restore_prev_latest; ${EX-}"
+		CLEANUP="[ -L \"\$LATEST_DIR_BACKUP\" ] && \
+		                 rm -- \"\$LATEST_DIR_BACKUP\"; \
+			 ${CLEANUP-}"
 	else
-		EX="srmlink \"\$LATEST_DIR\"; ${EX-}"
+		remove_latest() {
+			[ -L "$LATEST_DIR" ] || return
+			warn "Removing $B%s$R." "$(pphome "$LATEST_DIR")"
+			rm -- "$LATEST_DIR"
+		}
+		EX="remove_latest; ${EX-}"
 	fi
 	ln -fs "$REPO" "$LATEST_DIR"
 fi
+
 
 # Cleanup.
 warn "${GR}Installation complete.$R"
@@ -738,7 +543,7 @@ fi
 
 case $YN in
 	(0)	warn "Removing $B%s$R." "$PRETTY_REPO_DIR"
-		rm -rf "$REPO_DIR" ;;
+		rm -rf -- "$REPO_DIR" ;;
 	(1|2)	: ;;
 	(*)	warn "Did ${B}not${R} remove $B%s$R." "$PRETTY_REPO_DIR"
 esac
